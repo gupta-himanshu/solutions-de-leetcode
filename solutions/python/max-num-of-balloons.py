a4fa00705ab974eb3f988c6b963d79b1381a3b47
Himanshu Gupta <himanshu.gupta1@nashtechglobal.com>
2026-06-22 12:04:52 -0400

    added daily solution for leetcode problem #1189

---
+++ solutions/python/max-num-of-balloons.py
@@ -0,0 +1,41 @@
+"""
+Problem: 1189. Maximum Number of Balloons
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+
+Example 1:
+Input: text = "nlaebolko"
+Output: 1
+
+Example 2:
+Input: text = "loonbalxballpoon"
+Output: 2
+
+Example 3:
+Input: text = "leetcode"
+Output: 0
+
+Constraints:
+1 <= text.length <= 10^4
+text consists of lower case English letters only.
+"""
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        if len(text) == 0:
+            return 0
+
+        balloon_str = "balloon"
+        balloon_dict = {}
+
+        for i in range(len(balloon_str)):
+            balloon_dict[balloon_str[i]] = 0
+
+        for i in range(len(text)):
+            if text[i] in balloon_str:
+                balloon_dict[text[i]] += 1
+            else:
+                pass
+
+        balloon_dict["l"] = balloon_dict["l"] // 2
+        balloon_dict["o"] = balloon_dict["o"] // 2
+        return min(balloon_dict.values())
