d6e8b219a325a4020e7d903a6a6d496201d44a5b
Himanshu Gupta <himanshu.gupta1@nashtechglobal.com>
2026-07-06 15:01:20 -0400

    added python solution for problem 1288 - remove covered intervals

---
+++ solutions/python/remove-covered-intervals.py
@@ -0,0 +1,36 @@
+"""
+Problem: 1288. Remove Covered Intervals
+Given an array of intervals where intervals[i] = [li, ri] represent the interval [li, ri), remove all intervals that are covered by another interval in the list.
+An interval [a, b) is covered by an interval [c, d) if and only if c <= a and b <= d. Return the number of remaining intervals.
+
+Example 1:
+Input: intervals = [[1,4],[3,6],[2,8]]
+Output: 2
+Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+
+Example 2:
+Input: intervals = [[1,4],[2,3]]
+Output: 1
+Explanation: Interval [2,3] is covered by [1,4], therefore it is removed.
+
+Constraints:
+1 <= intervals.length <= 1000
+0 <= li < ri <= 10^5
+"""
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        n = len(intervals)
+        result = n
+
+        for i in range(n):
+            for j in range(n):
+                if j == i:
+                    continue
+                else:
+                    if intervals[j][0] <= intervals[i][0] and intervals[i][1] <= intervals[j][1]:
+                        result -= 1
+                        break
+                    else:
+                        continue
+
+        return result
